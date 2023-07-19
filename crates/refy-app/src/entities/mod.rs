c86fe4ce763e47497f7fcabbfda594193c4f5890
osamamaqtari <almaktary.osama@gmail.com>
2023-07-19 09:08:54 +0300

    feat(core): add `VerifierTags` entity

--- crates/refy-app/src/entities/mod.rs
+++ crates/refy-app/src/entities/mod.rs
@@ -3,8 +3,11 @@ mod ticket;
 mod ticket_tags;
 mod verified_tickets;
 mod verifier;
+mod verifier_tags;
+
 pub use tag::Tag;
 pub use ticket::Ticket;
 pub use ticket_tags::TicketTags;
 pub use verified_tickets::VerifiedTickets;
 pub use verifier::Verifier;
+pub use verifier_tags::VerifierTags;
---
+++ crates/refy-app/src/entities/verifier_tags.rs
@@ -0,0 +1,23 @@
+use chrono::{DateTime, Utc};
+use reddd::domain::{Entity, Key};
+use reddd_macros::Entity;
+use serde::{Deserialize, Serialize};
+
+use super::{Tag, Verifier};
+
+/// A struct representing a verifier tags.
+#[derive(Clone, Debug, Entity, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct VerifierTags {
+    /// The unique identifier of the verifier tags.
+    pub id: Key<Self, uuid::Uuid>,
+
+    /// The verifier id.
+    pub verifier_id: <Verifier as Entity>::Key,
+
+    /// The tag id.
+    pub tag_id: <Tag as Entity>::Key,
+
+    /// The timestamp at which the ticket tags was created.
+    pub created_at: DateTime<Utc>,
+}
